14f76fbbf143dccff6ab93fd5b5e7d7e7666c73b
yuthika mishra <mishrayuthika05@gmail.com>
2026-03-21 23:46:53 +0530

    Change transaction file path and create directory
    Updated transaction file path and ensured directory exists.

--- src/tracker.py
+++ src/tracker.py
@@ -1,39 +1,6 @@
-import pandas as pd
-from datetime import datetime
-from src.predictor import predict_category
+import os
 
-FILE = "data/user_transactions.csv"
+FILE = "data/transactions.csv"
 
-def add_transaction(amount, description):
-
-    category = predict_category(description)
-
-    transaction = {
-        "date": datetime.today().strftime("%Y-%m-%d"),
-        "amount": amount,
-        "description": description,
-        "category": category
-    }
-
-    df_new = pd.DataFrame([transaction])
-
-    try:
-        df_old = pd.read_csv(FILE)
-        df = pd.concat([df_old, df_new])
-    except:
-        df = df_new
-
-    df.to_csv(FILE, index=False)
-
-    print("\nTransaction Added")
-    print("Predicted Category:", category)
-
-
-def show_transactions():
-
-    try:
-        df = pd.read_csv(FILE)
-        print("\nTransactions:\n")
-        print(df)
-    except:
-        print("No transactions found.")
+# ensure folder exists
+os.makedirs(os.path.dirname(FILE), exist_ok=True)
