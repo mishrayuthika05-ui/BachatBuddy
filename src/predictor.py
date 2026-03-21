7f841a1a05e7118fe2a6e01d637e51d68a138259
yuthika mishra <mishrayuthika05@gmail.com>
2026-03-21 23:42:05 +0530

    Update predictor.py

--- src/predictor.py
+++ src/predictor.py
@@ -1,7 +1,2 @@
-import pickle
-
-model = pickle.load(open("models/model.pkl", "rb"))
-
 def predict_category(text):
-    category = model.predict([text])
-    return category[0]
+    return "General"
