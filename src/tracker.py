29d9c9af40545170ccc2d55d214f20289c0b604c
yuthika mishra <mishrayuthika05@gmail.com>
2026-03-22 18:38:08 +0530

    Update tracker.py

--- src/tracker.py
+++ src/tracker.py
@@ -1,6 +1,34 @@
+import pandas as pd
 import os
+from predictor import predict_category
 
 FILE = "data/transactions.csv"
 
 # ensure folder exists
 os.makedirs(os.path.dirname(FILE), exist_ok=True)
+
+def add_transaction(amount, description):
+    category = predict_category(description)
+
+    new_data = {
+        "amount": amount,
+        "description": description,
+        "category": category
+    }
+
+    try:
+        df = pd.read_csv(FILE)
+    except:
+        df = pd.DataFrame(columns=["amount", "description", "category"])
+
+    df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
+    df.to_csv(FILE, index=False)
+
+def show_transactions():
+    try:
+        df = pd.read_csv(FILE)
+        import streamlit as st
+        st.write(df)
+    except:
+        import streamlit as st
+        st.write("No data found")
